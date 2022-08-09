267007ae666c1a0206ffac98dce5a75af6279f0b
Aleksei Ents <99408143+djo00771@users.noreply.github.com>
2022-08-09 15:05:41 +0300

    update elements tests add test_radio_button

--- locators/element_page_locators.py
+++ locators/element_page_locators.py
@@ -4,14 +4,12 @@ from selenium.webdriver.common.by import By
 class TextBoxPageLocators:
     """ Локаторы для страницы Text Box"""
 
-    # Для отправки данных
     FULL_NAME = (By.CSS_SELECTOR, '#userName')
     EMAIL = (By.CSS_SELECTOR, '#userEmail')
     CURRENT_ADDRESS = (By.CSS_SELECTOR, '#currentAddress')
     PERMANENT_ADDRESS = (By.CSS_SELECTOR, '#permanentAddress')
     SUBMIT = (By.CSS_SELECTOR, 'button[id="submit"]')
 
-    # Для получения данных
     CREATED_FULL_NAME = (By.CSS_SELECTOR, '#output #name')
     CREATED_EMAIL = (By.CSS_SELECTOR, '#output #email')
     CREATED_CURRENT_ADDRESS = (By.CSS_SELECTOR, '#output #currentAddress')
@@ -27,3 +25,11 @@ class CheckBoxPageLocators:
     TITLE_ITEM = ".//ancestor::span[@class='rct-text']"
     OUTPUT_RESULT = (By.CSS_SELECTOR, 'span[class="text-success"]')
 
+
+class RadioButtonPageLocators:
+    """ Локаторы для страницы Radio Button """
+
+    YES_RADIOBUTTON = (By.CSS_SELECTOR, 'label[class^="custom-control"][for="yesRadio"]')
+    IMPRESSIVE_RADIOBUTTON = (By.CSS_SELECTOR, 'label[class^="custom-control"][for="impressiveRadio"]')
+    NO_RADIOBUTTON = (By.CSS_SELECTOR, 'label[class^="custom-control"][for="noRadio"]')
+    OUTPUT_RESULT = (By.CSS_SELECTOR, 'p span[class="text-success"]')
--- pages/elements_page.py
+++ pages/elements_page.py
@@ -3,7 +3,7 @@ import time
 
 import locators.element_page_locators
 from generator.generator import generated_person
-from locators.element_page_locators import TextBoxPageLocators, CheckBoxPageLocators
+from locators.element_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators
 from pages.base_page import BasePage
 
 
@@ -71,3 +71,19 @@ class CheckBoxPage(BasePage):
         for item in result_list:
             data.append(item.text)
         return str(data).replace(' ', '').lower()
+
+
+class RadioButtonPage(BasePage):
+
+    locators = RadioButtonPageLocators()
+
+    def click_on_the_radio_button(self, choice):
+        """ Кликнуть по кнопкам """
+        choices = {'yes': self.locators.YES_RADIOBUTTON,
+                   'impressive': self.locators.IMPRESSIVE_RADIOBUTTON,
+                   'no': self.locators.NO_RADIOBUTTON}
+
+        self.is_visible(choices[choice]).click()
+
+    def get_output_result(self):
+        return self.is_present(self.locators.OUTPUT_RESULT).text
